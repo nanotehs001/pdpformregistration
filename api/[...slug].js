3ec09799381fd3e519a78dd8f5114d15915b041f
nanotehs001 <kurl.b1995@gmail.com>
2026-08-03 17:24:19 +0800

    Fix serverless handler to restore /api prefix for Express routes

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,3 +1,8 @@
 import app from '../server/src/app.js';
 
-export default app;
+export default function handler(req, res) {
+  // Vercel strips /api from the path, so we need to prepend it back
+  // so that Express routes work correctly.
+  req.url = `/api${req.url}`;
+  return app(req, res);
+}
